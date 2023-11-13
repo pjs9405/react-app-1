17533d3d6fbbea5ecfe18e73cb6b57b9e50ccf8a
junseong <pjs3948@gmail.com>
2023-11-13 13:45:30 +0900

    arrow function 사용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import { useState } from 'react';
 
 function Counter({title,initValue}) {  
+  const [count,setCount] = useState(initValue);
+
+  const up = () => setCount(count+1);
+  const down = () => setCount(count-1);
+  
   return <><h1>{title}</h1>
-  <button>+</button> {initValue}</>
+  <button onClick={up}>+</button>
+  <button onClick={down}>-</button> {count}</>
 }
 
 function App() {
